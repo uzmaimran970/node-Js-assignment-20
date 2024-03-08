91330478799b5fdc083d3f4f75a719959d38dbaa
Uzma imran <148060110+uzmaimran970@users.noreply.github.com>
2024-03-08 16:37:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let items = ["pakistan", "karachi", "indus", "urdu"];
+console.log(items);
+// dot push method in array
+let langauges = [];
+langauges.push("urdu");
+langauges.push("english");
+langauges.push("french");
+langauges.push("chainees");
+console.log(langauges);
